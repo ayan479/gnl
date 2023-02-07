d39637c11cf8abe10f59db8f4886b34b0a3a61fb
Mohammad Ayan <mayan@lab1r2s6.42abudhabi.ae>
2023-02-07 14:11:18 +0400

    test

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mayan <mayan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 13:19:04 by mayan             #+#    #+#             */
+/*   Updated: 2023/02/02 15:55:55 by mayan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+
+char *get_next_line(int fd);
+
+
+#endif
