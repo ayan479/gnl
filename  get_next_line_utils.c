1aef263ded90746bbfb6ccfc90b1cbebcade5912
mayan <you@example.com>
2023-02-07 19:14:12 +0400

    test

---  get_next_line_utils.c
+++  get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: mayan <mayan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 13:18:37 by mayan             #+#    #+#             */
-/*   Updated: 2023/02/07 14:21:01 by mayan            ###   ########.fr       */
+/*   Updated: 2023/02/07 18:21:15 by mayan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,56 @@ size_t	ft_strlen(const char *str)
 	}
 	return (i);
 }
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char	uc;
+
+	uc = c;
+	while (*s)
+	{
+		if (*s == uc)
+			return ((char *) s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (!dst && !src)
+	{
+		return (NULL);
+	}
+	while (i < n)
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dst);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		len1;
+	int		len2;
+	char	*tab;
+
+	if (!s1 ||!s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	tab = malloc(len1 + len2 + 1);
+	if (!tab)
+		return (NULL);
+	ft_memcpy(tab, s1, len1);
+	ft_memcpy(tab + len1, s2, len2 + 1);
+	return (tab);
+}
+
+                                                                                                                                                                                                                                                                                                                                        
